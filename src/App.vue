cd44d579e05b3d26f2879e003b7939e82ac212d0
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-05 20:04:17 +0100

    Appending navbar, Sidebar and ImageContainer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  
+  <Navbar />
+  <main class="h-screen flex divide-x-2">
+    <Sidebar class="w-1/3 h-full bg-red-300" />
+    <ImageContainer class="w-2/3 h-full bg-red-300" />
+  </main>
 </template>
 
 <script>
-
+import Navbar from '@/components/header/Navbar';
+import Sidebar from './components/sidebar/Sidebar.vue';
+import ImageContainer from './components/container/ImageContainer';
 export default {
   name: 'App',
   components: {
+    Navbar,
+    Sidebar,
+    ImageContainer,
   }
 }
 </script>
