b40e62d32f557dbeed855e9944c1d6ecb0034d51
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-06 19:35:41 +0100

    Control Panel where we can filter and load images

---
+++ src/components/sidebar/ControlPanel.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <aside class="divide-y">
+        <ImageLoader />
+        <FilterOptions />
+    </aside>
+
+</template>
+
+<script>
+import ImageLoader from '@/components/sidebar/ImageLoader';
+import FilterOptions from '@/components/sidebar/FilterOptions';
+export default {
+    name: 'ControlPanel',
+    components: {
+        ImageLoader,
+        FilterOptions,
+    }
+}
+</script>
+
+<style>
+
+</style>
