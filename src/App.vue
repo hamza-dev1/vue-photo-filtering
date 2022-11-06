5d8eeda150c56e14bf4bb64975bbb1c4c019257f
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-06 19:54:51 +0100

    rename Sidebar component to ControlPanel and modify layout

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <Navbar />
   <main class="h-screen flex divide-x-2">
-    <Sidebar class="w-1/3 h-full bg-red-300" />
-    <ImageContainer class="w-2/3 h-full bg-red-300" />
+    <ControlPanel class="w-1/6 h-full bg-red-300" />
+    <ImageContainer class="w-5/6 h-full bg-red-300" />
   </main>
 </template>
 
 <script>
 import Navbar from '@/components/header/Navbar';
-import Sidebar from './components/sidebar/Sidebar.vue';
+import ControlPanel from './components/sidebar/ControlPanel';
 import ImageContainer from './components/container/ImageContainer';
 export default {
   name: 'App',
   components: {
     Navbar,
-    Sidebar,
+    ControlPanel,
     ImageContainer,
   }
 }
