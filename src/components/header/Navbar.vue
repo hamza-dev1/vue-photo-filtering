cc5f886972c8f3c7368e92c2ee49fa259a5e8962
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-06 19:55:24 +0100

    remove rounded class

--- src/components/header/Navbar.vue
+++ src/components/header/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
   <div class="container flex flex-wrap justify-between items-center mx-auto">
     <a href="#" class="flex items-center">
         <img src="../../assets/images/logo.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
