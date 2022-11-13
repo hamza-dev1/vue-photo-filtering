83ba8e568da22688bb159a3741f464cfab0d0ecf
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-13 11:52:06 +0100

    Add input for image source to be loaded

--- src/components/sidebar/ImageLoader.vue
+++ src/components/sidebar/ImageLoader.vue
@@ -1,15 +1,35 @@
 <template>
-  <div class="">
-    Image Loader
+  <div class="mb-6 p-5">
+    <label
+      for="image"
+      class="block mb-2 text-2xl font-medium text-white"
+      >Your image link</label
+    >
+    <input
+      type="text"
+      id="image"
+      @change="setImageSource"
+      v-model="imageSrc"
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      placeholder="image"
+      required
+    />
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
+  name: 'ImageLoader',
+  setup(props, context) {
 
-}
+    const imageSrc = ref('');
+
+    return {
+      imageSrc,
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
