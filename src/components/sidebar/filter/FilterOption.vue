96e4295e0c2c95a95673a83ce298f2690f7ad760
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-13 19:47:39 +0100

    New folder contains filter related components

---
+++ src/components/sidebar/filter/FilterOption.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="my-5">
+    <label
+      :for="filter.name"
+      class="block mb-2 text-sm font-medium text-white"
+      >{{ filter.name }}</label
+    >
+    <input
+      v-model="filter.value"
+      @change="filterValueUpdated"
+      @input="filterValueUpdated"
+      :min="filter.min"
+      :max="filter.max"
+      :id="filter.name"
+      type="range"
+      class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FilterOption',
+    props: ['filter', ],
+    setup(props) {
+        const filterValueUpdated = () => {
+            // ...
+        }
+
+        return {
+            filterValueUpdated
+        }
+    }
+};
+</script>
+
+<style></style>
---
+++ src/components/sidebar/filter/FiltersPanel.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="p-5">
+        <h1 class="text-2xl text-white mb-4">
+            Available Filters
+        </h1>
+        <FilterOption 
+            v-for="(filter, i) in filters" 
+            :key="i"
+            :filter="filter" />
+    </div>
+</template>
+
+<script>
+
+import { ref } from 'vue';
+import FilterOption from './FilterOption';
+import { FiltersService } from '@/services/FiltersService';
+
+export default {
+    name: 'FiltersPanel',
+    components: {
+        FilterOption
+    },
+    setup() {
+        const filters = ref([
+            ...FiltersService.getAllFilters()
+        ]);
+        
+        return {
+            filters
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
