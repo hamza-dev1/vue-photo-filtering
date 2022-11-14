a161dcb36a2a16467f1fd86330ededf59ee8e9cf
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-14 12:52:35 +0100

    define new mutation and an action for it

--- src/store/modules/filter.module.js
+++ src/store/modules/filter.module.js
@@ -4,9 +4,13 @@ export default {
         imageSource: ""
     },
     mutations: {
-
+        SET_IMAGE_SOURCE: (state, payload) => {
+            state.imageSource = payload.value;
+        }
     },
     actions: {
-
+        setImageSource: ({ commit }, payload) => {
+            return commit('SET_IMAGE_SOURCE', payload);
+        }
     }
 }
