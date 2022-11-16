b796eea52cc91077315c92b919364f15dc9eac98
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-16 14:14:53 +0100

    Move mutation and action to image module

--- src/store/modules/filter.module.js
+++ src/store/modules/filter.module.js
@@ -4,13 +4,8 @@ export default {
         imageSource: ""
     },
     mutations: {
-        SET_IMAGE_SOURCE: (state, payload) => {
-            state.imageSource = payload.value;
-        }
+
     },
     actions: {
-        setImageSource: ({ commit }, payload) => {
-            return commit('SET_IMAGE_SOURCE', payload);
-        }
     }
 }
