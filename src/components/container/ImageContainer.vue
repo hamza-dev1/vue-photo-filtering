9ab0580d4d3d5ef3109a2dc1823fdcd7a687fdf4
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-16 14:17:25 +0100

    Append child component SubNavBar and refactor js code

--- src/components/container/ImageContainer.vue
+++ src/components/container/ImageContainer.vue
@@ -1,50 +1,45 @@
 <template>
-    <section class="flex justify-center items-center">
-    <img 
+  <section class="">
+    <SubNavBar />
+    <div class="flex justify-center items-center">
+      <img
         :src="imageSrc"
-        alt="No image loaded!" 
-        class="image">
-
-    </section>
+        alt="No image loaded!"
+        class="image w-full h-full object-contain"
+      />
+    </div>
+  </section>
 </template>
 
 <script>
-
-import { ref, watch } from 'vue';
+import { ref, watch } from "vue";
 import { useStore } from "vuex";
+import SubNavBar from "../header/SubNavBar";
 
 export default {
-    // src="../../assets/images/logo.png" 
-    name: 'ImageContainer',
-    components: {
-    },
-    setup() {
-        const store = useStore();
-        const imageSrc = ref('');
+  name: "ImageContainer",
+  components: {
+    SubNavBar,
+  },
+  setup() {
+    const store = useStore();
+    const imageSrc = ref("");
+    
+    watch(store.getters.getImageState, () => {
+      imageSrc.value = store.getters.getImageState.image.source;
+    });
 
-        watch(store.getters.getFilterState, () => {
-            imageSrc.value = 
-                store.getters.getFilterState.imageSource
-        });
-
-        return {
-            imageSrc,
-        }
-    }
-}
-
+    return {
+      imageSrc,
+    };
+  },
+};
 </script>
 
 <style lang="postcss" scoped>
 .image {
-    filter: 
-        brightness(var(--brightness))
-        hue-rotate(var(--hue-rotate))
-        grayscale(var(--grayscale))
-        contrast(var(--contrast))
-        saturate(var(--saturate))
-        invert(var(--invert))
-        sepia(var(--sepia))
+  filter: brightness(var(--brightness)) hue-rotate(var(--hue-rotate))
+    grayscale(var(--grayscale)) contrast(var(--contrast))
+    saturate(var(--saturate)) invert(var(--invert)) sepia(var(--sepia));
 }
-
 </style>
