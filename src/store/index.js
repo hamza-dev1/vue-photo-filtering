dbe7b5b3f4ad24aa23235bbe2d0e0859b4070336
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-16 14:14:21 +0100

    Add state and getter and module for image

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,24 @@
-import { createStore } from 'vuex';
+import { createStore } from 'vuex'
 import filterModule from '@/store/modules/filter.module'
+import imageModule from '@/store/modules/image.module'
 
 export default createStore({
     state: {
-        filterState: filterModule.state
+        filterState: filterModule.state,
+        imageState: imageModule.state,
     },
     getters: {
         getFilterState: (state) => {
             return state.filterState;
         },
+        getImageState: (state) => {
+            return state.imageState;
+        }
     },
     mutations: {},
     actions: {},
     modules: {
-        filterModule
+        filterModule,
+        imageModule
     }
 })
